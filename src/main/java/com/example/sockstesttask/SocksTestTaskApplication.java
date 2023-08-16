b45e66e9685475284e936a8fefbb94bbed133faf
r0mpll <ilya.trubnikov.02@gmail.com>
2023-08-16 18:00:22 +0300

    Initial commit

---
+++ src/main/java/com/example/sockstesttask/SocksTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.sockstesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksTestTaskApplication.class, args);
+	}
+
+}
